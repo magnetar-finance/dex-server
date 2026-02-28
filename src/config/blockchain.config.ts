cc3e14289b3a3e9963110d0a4d5ae21fb8d6c4cd
Kingsley Victor <javaprodigy56@gmail.com>
2026-02-28 20:56:51 +0100

    chore: made updates

--- src/config/blockchain.config.ts
+++ src/config/blockchain.config.ts
@@ -16,11 +16,11 @@ function loadChainInfo(): ChainConnectionInfo[] {
       rpcInfos: [
         {
           url: 'https://atlantic.dplabs-internal.com',
-          queryBlockRange: 300,
+          queryBlockRange: 700,
         },
         {
           url: 'https://api.zan.top/node/v1/pharos/atlantic/5c34a51f74114a7a9c201593fb45a73e',
-          queryBlockRange: 300,
+          queryBlockRange: 700,
         },
       ],
       chainId: ChainIds.PHAROS_TESTNET,
@@ -29,7 +29,7 @@ function loadChainInfo(): ChainConnectionInfo[] {
       rpcInfos: [
         {
           url: 'https://gcp-2.seismictest.net/rpc',
-          queryBlockRange: 300,
+          queryBlockRange: 30000,
         },
       ],
       chainId: ChainIds.SEISMIC_TESTNET,
